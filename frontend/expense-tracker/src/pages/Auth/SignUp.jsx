436187044d30343ea6db0080f0357eb9b64e0e3c
Navroj <navroj9994@gmail.com>
2025-06-28 17:15:08 +0530

    commit

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,10 +1,20 @@
 import React,{ useState } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useAsyncError, useNavigate } from 'react-router-dom';
 import Input from '../../components/Inputs/Input';
 import { validateEmail } from '../../utils/helper';
 
 const SignUp = () => {
+  const [profilePic,setProfilePic] = useState(null);
+  const [fullName,setFullName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+
+  const [error,setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  
   return (
     <div>SignUp</div>
   )
