42b446bd3445d23c9be1991b6176c1d8f065a078
Navroj <navroj9994@gmail.com>
2025-06-28 17:40:31 +0530

    commit-2

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,23 +1,59 @@
-import React,{ useState } from 'react';
-import AuthLayout from '../../components/layouts/AuthLayout';
-import { Link, useAsyncError, useNavigate } from 'react-router-dom';
-import Input from '../../components/Inputs/Input';
-import { validateEmail } from '../../utils/helper';
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { Link, useAsyncError, useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
 const SignUp = () => {
-  const [profilePic,setProfilePic] = useState(null);
-  const [fullName,setFullName] = useState("");
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [error,setError] = useState(null);
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
-  
-  return (
-    <div>SignUp</div>
-  )
-}
+  const handleSignUp = async (e) => {};
 
-export default SignUp
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+        <form onSubmit={handleSignUp}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+            />
+
+            <Input
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+            <div className="col-span-2">
+              <Input
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+                label="Password"
+                placeholder="Minimum 8 characters"
+                type="password"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default SignUp;
