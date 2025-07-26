18e1c057fa66d91965dab8e373a14b0a285c5f65
Navroj <navroj9994@gmail.com>
2025-07-26 18:49:07 +0530

    26/07

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -15,7 +17,7 @@ app.use(cors(
 
 app.use(express.json());
 
+connectDB();
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () =>  console.log(`Server is running on port ${PORT}`));
